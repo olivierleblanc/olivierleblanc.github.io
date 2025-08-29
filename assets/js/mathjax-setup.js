c15952383db12fdb037f4c402a877181bd9aba35
olivierleblanc <oliviero3@hotmail.com>
2025-08-29 15:35:37 +0200

    mod mathjax-setup (goes with blog posts)

--- assets/js/mathjax-setup.js
+++ assets/js/mathjax-setup.js
@@ -1,10 +1,15 @@
 window.MathJax = {
   tex: {
+    packages: {'[+]': ['ams']}, // Explicitly include 'base' for basic commands
     tags: "ams",
     inlineMath: [
       ["$", "$"],
       ["\\(", "\\)"],
     ],
+    macros: {
+      llbracket: '{\\mathopen{\\lbrack\\kern-0.15em\\lbrack}}',
+      rrbracket: '{\\mathclose{\\rbrack\\kern-0.15em\\rbrack}}'
+    },
   },
   options: {
     renderActions: {
